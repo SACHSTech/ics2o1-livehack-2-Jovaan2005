86ecb87df9afaf1e718bd3d9f4ad51707a1f3e6f
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2021-03-12 04:25:03 +0000

    submit

--- problem1.py
+++ problem1.py
@@ -1,1 +1,17 @@
+speed = int(input("Enter your speed: "))
+speed_limit = int(input("Enter the speed limit: "))
+fine = 0
+
+if (speed >= 1 + speed_limit and speed <=20 + speed_limit):
+    fine = 100
+    print("Your fine is $" + str(fine))
+elif (speed >= 21 + speed_limit and speed <=30 + speed_limit):
+    fine = 270
+    print("Your fine is $" + str(fine))
+elif (speed >= 31 + speed_limit):
+    fine = 570
+    print("Your fine is $" + str(fine))
+else:
+    print("You're driving safe! You were under the speed limit")
+
 
--- problem2.py
+++ problem2.py
@@ -1,1 +1,14 @@
+angle1 = int(input(" Enter in length of side 1:  "))
+angle2 = int(input("Enter the length of side 2: "))
+angle3 = int(input("Enter the length of side 3: "))
 
+an1 = angle1**2   
+an2 = angle2**2
+an3 = angle3**2
+
+if (an1+an2 == an3) or (an1+an3 == an2)  or (an2+an3 == an1):
+  print("The figure is a triangle " + str(angle1) + "," +
+  str(an2) + ", and " + str(an3) + " The figure is a triangle..")
+else:
+    print("A triangle with lengths " + str(angle1) + ", " +
+    str(angle2) + ", and " + str(angle3) + " The figure is NOT a triangle.")
